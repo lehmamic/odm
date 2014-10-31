4704a13d25924c94fedd8d08dcd930a0e47f6b07
lehmamic <lehmamic@bluewin.ch>
2014-10-31 14:02:55 +0100

    Initial Draft

---
+++ src/Odm.Server.Host/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+
+namespace Odm.Server.Host
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
